b20d6a0f12e39c41f78975ab4c5cba8da43cea19
Year <1754146628@qq.com>
2022-10-01 16:32:42 +0000

    commit

--- src/newdimension/content/CrafterBlocks.java
+++ src/newdimension/content/CrafterBlocks.java
@@ -2,12 +2,14 @@ package newdimension.content;
 
 import mindustry.world.*;
 import mindustry.world.blocks.production.*;
+import mindustry.content.Items;
 import mindustry.type.*;
 
 public class CrafterBlocks implements ContentList{
     
     public static Block steelFactory,
-    electricCubeFactory;
+    electricCubeFactory,
+    hypernuclearCondensatesFactory;
 
     @Override
     public void load(){
@@ -34,6 +36,22 @@ public class CrafterBlocks implements ContentList{
             itemCapacity = 20;
             consumes.power(6.5f);
             outputItem = new ItemStack(NDItems.electricCube, 2);
+        }};
+
+        hypernuclearCondensatesFactory = new GenericCrafter("hypernuclear-condensates-factory"){{
+            requirements(Category.crafting, ItemStack.with(NDItems.steel, 20, NDItems.iron, 30));
+            size = 3;
+            health = 2000;
+            craftTime = 120f;
+            hasItems = true;
+            hasPower = true;
+            itemCapacity = 20;
+            consumes.items(
+                new ItemStack(Items.surgeAlloy, 1),
+                new ItemStack(Items.thorium, 2)
+            );
+            consumes.power(7f);
+            outputItem = new ItemStack(NDItems.hypernuclearCondensates, 2);
         }};
     }
 }
