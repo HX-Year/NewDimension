beea048ba6dcde1326513cfef6ae28e3d8050dee
HX-Year <1754146628@qq.com>
2022-12-04 09:26:01 +0800

    commit

--- src/newdimension/content/NDPlanets.java
+++ src/newdimension/content/NDPlanets.java
@@ -5,8 +5,11 @@ import mindustry.content.*;
 import mindustry.game.*;
 import mindustry.graphics.*;
 import mindustry.graphics.g3d.*;
-import mindustry.maps.planet.*;
+import mindustry.maps.planet.ErekirPlanetGenerator;
 import mindustry.type.*;
+import mindustry.world.meta.Attribute;
+import mindustry.world.meta.Env;
+import newdimension.world.gen.NDPlanetGenerator;
 
 public class NDPlanets implements ContentList{
 
@@ -30,36 +33,45 @@ public class NDPlanets implements ContentList{
                 Color.valueOf("f4ee8e")
             );
         }};*/
-
         ND = new Planet("ND", Planets.sun, 2f, 3){{
-            generator = new SerpuloPlanetGenerator();
-            meshLoader = () -> new HexMesh(this, 6);
+            generator = new NDPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 5);
             cloudMeshLoader = () -> new MultiMesh(
-                new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
-                new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
+                    new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
+                    new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("eea293").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
             );
-            launchCapacityMultiplier = 0.5f;
-            sectorSeed = 2;
-            allowWaves = true;
-            allowWaveSimulation = true;
-            allowSectorInvasion = true;
-            allowLaunchSchematics = true;
-            enemyCoreSpawnReplace = true;
-            allowLaunchLoadout = true;
-            prebuildBase = false;
-            ruleSetter = r -> {
-                r.waveTeam = Team.crux;
-                r.placeRangeCheck = false;
-                r.attributes.clear();
-                r.showSpawns = false;
-            };
-            atmosphereColor = Color.valueOf("3c1b8f");
+            alwaysUnlocked = true;
+            landCloudColor = Color.valueOf("ed6542");
+            atmosphereColor = Color.valueOf("f07218");
+            defaultEnv = Env.scorching | Env.terrestrial;
+            startSector = 10;
             atmosphereRadIn = 0.02f;
             atmosphereRadOut = 0.3f;
-            startSector = 15;
-            alwaysUnlocked = true;
-            landCloudColor = Pal.spore.cpy().a(0.5f);
+            tidalLock = true;
+            orbitSpacing = 2f;
+            totalRadius += 2.6f;
+            lightSrcTo = 0.5f;
+            lightDstFrom = 0.2f;
+            clearSectorOnLose = true;
+            defaultCore = NDCoreBlocks.NDcore;
             hiddenItems.addAll(NDItems.ndItems).removeAll(Items.serpuloItems);
+
+            //TODO SHOULD there be lighting?
+            updateLighting = false;
+
+            ruleSetter = r -> {
+                r.waveTeam = Team.malis;
+                r.placeRangeCheck = false; //TODO true or false?
+                r.attributes.set(Attribute.heat, 0.8f);
+                r.showSpawns = true;
+                r.fog = true;
+                r.staticFog = true;
+                r.lighting = false;
+                r.coreDestroyClear = true;
+                r.onlyDepositCore = true; //TODO not sure
+            };
+
+            unlockedOnLand.add(NDCoreBlocks.NDcore);
         }};
     }
 }
--- src/newdimension/content/NDTechTree.java
+++ src/newdimension/content/NDTechTree.java
@@ -11,68 +11,66 @@ import static newdimension.content.NDTurrets.*;
 public class NDTechTree implements ContentList{
     @Override
     public void load(){
-        {
-            NDPlanets.ND.techTree = nodeRoot("ND", NDcore, () -> {
-                node(ironWall, () -> {
-                    node(ironWallLarge, () -> {});
-                    node(aluminiumWall, () -> {
-                        node(aluminiumWallLarge, () -> {});
+        NDPlanets.ND.techTree = nodeRoot("ND", NDcore, () -> {
+            node(ironWall, () -> {
+                node(ironWallLarge, () -> {});
+                node(aluminiumWall, () -> {
+                    node(aluminiumWallLarge, () -> {});
+                });
+                node(steelWall, () -> {
+                    node(steelWallLarge, () -> {});
+                    node(goldWall, () -> {
+                        node(goldWallLarge, () -> {});
                     });
-                    node(steelWall, () -> {
-                        node(steelWallLarge, () -> {});
-                        node(goldWall, () -> {
-                            node(goldWallLarge, () -> {});
-                        });
-                        node(tungstenWall, () -> {
-                            node(tungstenWallLarge, () -> {
-                                node(tungstenSteelWall, () -> {
-                                    node(tungstenSteelWallLarge, () -> {});
-                                });
+                    node(tungstenWall, () -> {
+                        node(tungstenWallLarge, () -> {
+                            node(tungstenSteelWall, () -> {
+                                node(tungstenSteelWallLarge, () -> {});
                             });
                         });
                     });
-                });
-
-                nodeProduce(NDItems.iron, () -> {
-                    nodeProduce(NDItems.steel, () -> {
-                        nodeProduce(NDItems.tungstenSteel, () -> {
-                            nodeProduce(NDItems.radioactiveSubstances, () -> {
-                                nodeProduce(NDItems.hypernuclearCondensates, () -> {});
-                            });
-                            nodeProduce(NDItems.electricCube, () -> {
-                                nodeProduce(NDItems.refactoringModule, () -> {});
-                            });
-                            nodeProduce(NDItems.iterativeModule, () -> {});
-                        });
-                    });
-                    nodeProduce(NDItems.aluminium, () -> {
-                        nodeProduce(NDItems.gold, () -> {});
-                    });
-                });
-
-                nodeProduce(NDLiquids.lava, () -> {
-                    nodeProduce(NDLiquids.nuclearWastewater, () -> {
-                        nodeProduce(NDLiquids.superFrozenLiquid, () -> {});
-                    });
-                });
-
-                node(steelFactory, () -> {
-                    node(tungstenSteelSmelter, () -> {
-                        node(electricCubeBaler, () -> {
-                            node(superNuclearMachine, () -> {});
-                        });
-                    });
-                });
-
-                node(batteryStack, () -> {
-                    node(batteryStackLarge, () -> {});
-                });
-
-                node(huiCan, () -> {
-                    node(superconductingElectromagneticGun, () -> {});
-                    node(frost, () -> {});
                 });
             });
-        }
-    };
+
+            nodeProduce(NDItems.iron, () -> {
+                nodeProduce(NDItems.steel, () -> {
+                    nodeProduce(NDItems.tungstenSteel, () -> {
+                        nodeProduce(NDItems.radioactiveSubstances, () -> {
+                            nodeProduce(NDItems.hypernuclearCondensates, () -> {});
+                        });
+                        nodeProduce(NDItems.electricCube, () -> {
+                            nodeProduce(NDItems.refactoringModule, () -> {});
+                        });
+                        nodeProduce(NDItems.iterativeModule, () -> {});
+                    });
+                });
+                nodeProduce(NDItems.aluminium, () -> {
+                    nodeProduce(NDItems.gold, () -> {});
+                });
+            });
+
+            nodeProduce(NDLiquids.lava, () -> {
+                nodeProduce(NDLiquids.nuclearWastewater, () -> {
+                    nodeProduce(NDLiquids.superFrozenLiquid, () -> {});
+                });
+            });
+
+            node(steelFactory, () -> {
+                node(tungstenSteelSmelter, () -> {
+                    node(electricCubeBaler, () -> {
+                        node(superNuclearMachine, () -> {});
+                    });
+                });
+            });
+
+            node(batteryStack, () -> {
+                node(batteryStackLarge, () -> {});
+            });
+
+            node(huiCan, () -> {
+                node(superconductingElectromagneticGun, () -> {});
+                node(frost, () -> {});
+            });
+        });
+    }
 }
