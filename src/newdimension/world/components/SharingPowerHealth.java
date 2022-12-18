148ee272066544f615067954fb18bd9583f453e1
HX-Year <1754146628@qq.com>
2022-12-18 15:16:44 +0800

    commit

---
+++ src/newdimension/world/components/SharingPowerHealth.java
@@ -0,0 +1,11 @@
+package newdimension.world.components;
+
+import newdimension.world.blocks.power.NDPowerNode;
+
+public class SharingPowerHealth extends ComponentBase<NDPowerNode.NDPowerNodeBuild>{
+    @Override
+    public void onUpdate(NDPowerNode.NDPowerNodeBuild nd){
+        float thisPH;
+        float otherPH;
+    }
+}
