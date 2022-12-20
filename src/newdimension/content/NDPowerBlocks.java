464ed2a8b684fbf1fb40038f64893d69a20d0e0c
Year <1754146628@qq.com>
2022-12-20 16:14:10 +0000

    commit

--- src/newdimension/content/NDPowerBlocks.java
+++ src/newdimension/content/NDPowerBlocks.java
@@ -6,6 +6,7 @@ import mindustry.type.ItemStack;
 import mindustry.world.Block;
 import mindustry.world.blocks.power.Battery;
 import mindustry.world.blocks.power.PowerNode;
+import newdimension.world.blocks.power.NDPowerNode;
 
 import static mindustry.type.ItemStack.with;
 
@@ -14,7 +15,8 @@ public class NDPowerBlocks implements ContentList{
     public static Block batteryStack,//电池堆
     batteryStackLarge,//大型电池堆
     powerHealNode,//电力恢复节点
-    powerGridNode;//电网节点
+    powerGridNode,//电网节点
+    healNode;//恢复节点
 
     @Override
     public void load(){
@@ -50,10 +52,28 @@ public class NDPowerBlocks implements ContentList{
         }};*/
 
         powerGridNode = new PowerNode("power-grid-node"){{
-            requirements(Category.power, with(Items.titanium, 5, Items.lead, 10, Items.silicon, 3));
+            requirements(Category.power, ItemStack.with(
+                Items.titanium, 5,
+                Items.lead, 10,
+                Items.silicon, 3
+            ));
             size = 2;
             maxNodes = 15;
             laserRange = 15f;
+        }};
+
+        healNode = new NDPowerNode("heal-node"){{
+            requirements(Category.power, ItemStack.with(
+                Items.silicon, 6,
+                NDItems.nanoAlloy, 12,
+                NDItems.steel, 8
+            ));
+            consumesPower = true;
+            consumePower(1.5f);
+            size = 2;
+            maxNodes = 8;
+            laserRange = 12f;
+            components.add(sharingPowerHealth);
         }};
     }
 }
--- src/newdimension/world/components/SharingPowerHealth.java
+++ src/newdimension/world/components/SharingPowerHealth.java
@@ -1,11 +1,20 @@
 package newdimension.world.components;
 
+import mindustry.gen.Building;
 import newdimension.world.blocks.power.NDPowerNode;
 
 public class SharingPowerHealth extends ComponentBase<NDPowerNode.NDPowerNodeBuild>{
     @Override
     public void onUpdate(NDPowerNode.NDPowerNodeBuild nd){
-        float thisPH;
-        float otherPH;
+        for(Building other : nd.proximity){
+            if(other.block != nd.block){
+                float thisH = nd.health;
+                float otherH = other.health;
+                if(thisH > 0 && otherH < thisH && otherH > 0){
+                    other.health += 1;
+                    nd.health -= 1;
+                }
+            }
+        }
     }
 }
